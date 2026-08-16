c7c2606011092b44f26903b665501f383c4c8169
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-08-17 00:29:16 +0530

    Add Landing Page

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,35 @@
+import '../App.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+import Logo from '../assets/logo.png';
+import HeroImage from '../assets/hero.png';
+
+export default function landing() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="landingPageContainer">
+            <nav>
+                <div className="navHeader">
+                    <img src={Logo} alt="Logo" style={{ width: '8rem', height: 'auto' }} />
+                </div>
+                <div className="navlist">
+                    <p onClick={() => navigate('/aljk23')}>Join as Guest</p>
+                    <p onClick={() => navigate('/auth')}>Login</p>
+                    <p onClick={() => navigate('/auth')}>Sign Up</p>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div style={{ width: '40%', marginLeft: '5rem' }}>
+                    <h1><span style={{ color: '#3a32f5' }}>Connect</span> with the World!</h1>
+                    <p>Join our community and connect with people from all over the world!</p>
+                    <div role="button"><Link to="/auth">Get Started</Link></div>
+                </div>
+                <div style={{ marginRight: '5rem', marginTop: '2rem' }}>
+                    <img src={HeroImage} alt="Hero" style={{ width: '56rem', height: 'auto' }} />
+                </div>
+            </div>
+        </div>
+    )
+}
