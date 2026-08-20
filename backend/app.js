c95341b6e7afb518a7c695c628a1f8e3ddd451fe
Saumya Khobragade <khobragadesaumya@gmail.com>
2026-08-21 01:01:42 +0530

    Update PRODUCTION Variables

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-import dns from 'dns';
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import express from "express";
 import dotenv from "dotenv";
@@ -18,7 +18,12 @@ const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://nexmeet.saumyakhobragade.dev",
+        credentials: true,
+    }),
+);
 app.use(express.json({ limit: "40mb" }));
 app.use(express.urlencoded({ limit: "40mb", extended: true }));
 
